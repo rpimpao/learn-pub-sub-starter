040631e6874609ef99a3ca467592d328467315fd
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2026-03-11 18:59:03 +0100

    initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
 
 func main() {
+	connStr := "amqp://guest:guest@localhost:5672"
+
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
 	fmt.Println("Starting Peril server...")
+
+	// wait for a signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Closing Peril server")
 }
